acf26e60cf1c47793801ca5785838bd128800a7d
GraziellaPereira <graziellapereira52@gmail.com>
2025-02-18 20:07:11 -0300

    adicionado exercicio 2 e 3

---
+++ Aula17-02-Exercicio02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula17_02_Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, r, s;
+            Console.WriteLine("Digite três números: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+
+            r = Math.Pow(a + b, 2);
+            s = Math.Pow(b + c, 2);
+
+            d = ((r + s) / 2);
+
+            Console.WriteLine($"O resultado é: {d:F2}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Aula18-02-Exercicio03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, media;
+            Console.WriteLine("Digite as notas do aluno: ");
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+            nota3 = double.Parse(Console.ReadLine());
+
+            media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)/10);
+
+            Console.WriteLine($"A média da nota do aluno é: {media:F2}");
+            Console.ReadKey();
+        }
+    }
+}
